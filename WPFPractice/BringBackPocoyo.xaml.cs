cd1db0aca2197d945fd77b4ff7625e981779cb93
Aseed Khokhar <71330292+aseedk@users.noreply.github.com>
2021-03-27 10:09:19 +0500

    Visual Programing

---
+++ WPFPractice/BringBackPocoyo.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFPractice
+{
+    public partial class BringBackPocoyo : Window
+    {
+        public BringBackPocoyo()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("First Image Clicked");
+        }
+        
+        private void ButtonBase_OnClick2(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Second Image Clicked");
+        }
+        
+        private void ButtonBase_OnClick3(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Third Image Clicked");
+        }
+    }
+}
